ed7055fbafbbc757fb3953e09911af08e0315d4e
taiyufeng <215180695@qq.com>
2022-07-18 10:36:35 +0800

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,18 +7,19 @@ class UsersController < ApplicationController
   end
 
   def index 
-    @users = User.paginate(page: params[:page], per_page: 10)
+    @users = User.where(activated: true).paginate(page: params[:page], per_page: 10)
   end
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url  and return unless @user.activated
   end
   def create
     @user = User.new(user_params)
       if @user.save # 处理注册成功的情况 
-        log_in @user
-        flash[:success] = "Welcome to the Sample App!"
-        redirect_to @user
+        @user.send_activation_email
+        flash[:info] = "Please check your email to activate your account."
+        redirect_to root_url
       else
         render 'new' 
       end
