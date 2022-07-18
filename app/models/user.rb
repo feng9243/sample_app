b8cf4d7a9380aa7aed606a131a5ac45882768c6a
taiyufeng <215180695@qq.com>
2022-07-18 11:36:44 +0800

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
     before_save :downcase_email
     before_create :create_activation_digest   
     validates :name, presence: true, length: { maximum: 50 }
@@ -24,31 +24,58 @@ class User < ApplicationRecord
         self.remember_token = User.new_token
         update_attribute(:remember_digest, User.digest(remember_token))
     end
+
     def authenticated?(remember_token)
         return false if remember_digest.nil?
         BCrypt::Password.new(remember_digest).is_password?(remember_token)  
     end
+
     # 忘记用户
     def forget
         update_attribute(:remember_digest, nil) 
     end
+
+    # 如果密码重设请求超时了，返回 true 
+    def password_reset_expired?
+        reset_sent_at < 2.hours.ago 
+    end
+
+    # 激活账户
     def activate
        update_columns(activated: true, activated_at: Time.zone.now)
     end
+
+    # 发送激活邮件
     def send_activation_email
         UserMailer.account_activation(self).deliver_now 
     end
+    
+    # 设置密码重设相关的属性 
+    def create_reset_digest
+        self.reset_token = User.new_token
+        update_attribute(:reset_digest,  User.digest(reset_token))
+        update_attribute(:reset_sent_at, Time.zone.now)
+    end
+
+    # 发送密码重设邮件
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now 
+    end
+
     # 把电子邮件地址转换成小写 
     def downcase_email
         self.email = email.downcase end
     # 创建并赋值激活令牌和摘要 
+
     def create_activation_digest
         self.activation_token  = User.new_token
         self.activation_digest = User.digest(activation_token)
     end
+
     def authenticated?(attribute, token)
         digest = send("#{attribute}_digest")
         return false if digest.nil? 
         BCrypt::Password.new(digest).is_password?(token)
     end
+
 end
