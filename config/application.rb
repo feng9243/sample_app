b835a4fa200a5c212ac684a52199d5249c0a3e07
taiyufeng <215180695@qq.com>
2022-07-18 17:56:52 +0800

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module SampleApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # 在使用 Ajax 处理的表单中添加真伪令牌 
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
