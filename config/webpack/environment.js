d47a900c2679f3eaed147ee480b7a5927f51b179
taiyufeng <215180695@qq.com>
2022-07-21 16:46:21 +0800

    delete vue

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,5 @@
 const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
+
 
 const webpack = require('webpack')
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
@@ -11,6 +10,4 @@ environment.plugins.append('Provide', new webpack.ProvidePlugin({
 }))
 
 
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
 module.exports = environment
